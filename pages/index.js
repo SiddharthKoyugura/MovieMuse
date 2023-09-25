6e880cafa36296c02b15968a47c05ad0e25ecc27
SiddharthKoyugra <koyugurasiddhardha@gmail.com>
2023-09-26 01:14:32 +0530

    added popular videos function

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,31 @@ import SectionCards from "@/components/card/SectionCards";
 import Navbar from "@/components/navbar/Navbar";
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import { getVideos } from "@/lib/videos";
+import { getVideos, getPopularVideos } from "@/lib/videos";
 
 export async function getServerSideProps() {
   const disneyVideos = await getVideos("disney trailer");
   const productivityVideos = await getVideos("productivity");
   const travelVideos = await getVideos("travel");
+  const popularVideos = await getPopularVideos("");
   // const popularVideos = await getVideos("disney trailer");
 
   return {
     props: {
-      disneyVideos, 
-      productivityVideos, 
-      travelVideos, 
-    }
-  }
+      disneyVideos,
+      productivityVideos,
+      travelVideos,
+      popularVideos,
+    },
+  };
 }
 
-export default function Home({ disneyVideos, productivityVideos, travelVideos }) {
-  
+export default function Home({
+  disneyVideos,
+  productivityVideos,
+  travelVideos,
+  popularVideos,
+}) {
   return (
     <div>
       <Head>
@@ -30,19 +36,25 @@ export default function Home({ disneyVideos, productivityVideos, travelVideos })
         </title>
       </Head>
 
-      <Navbar userName="Siddharth" />
+      <div className={styles.main}>
+        <Navbar userName="Siddharth" />
 
-      <Banner
-        title="Clifford the red dog"
-        subTitle="a very cute dog"
-        imgUrl="/static/banner.jpg"
-      />
+        <Banner
+          title="Clifford the red dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/banner.jpg"
+        />
 
-      <div className={styles.sectionWrapper}>
-        <SectionCards title="Disney" size="large" videos={disneyVideos} />
-        <SectionCards title="Travel" size="small" videos={travelVideos} />
-        <SectionCards title="Productivity" size="medium" videos={productivityVideos} />
-        {/* <SectionCards title="Popular" size="large" videos={disneyVideos} /> */}
+        <div className={styles.sectionWrapper}>
+          <SectionCards title="Disney" size="large" videos={popularVideos} />
+          <SectionCards title="Travel" size="small" videos={travelVideos} />
+          <SectionCards
+            title="Productivity"
+            size="medium"
+            videos={productivityVideos}
+          />
+          {/* <SectionCards title="Popular" size="large" videos={disneyVideos} /> */}
+        </div>
       </div>
     </div>
   );
